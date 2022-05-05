fcfe3f6e06ca2ff7218ae31e98258c0d031662de
Juan J Rodas <74572519+Rhodes-bit@users.noreply.github.com>
2022-05-04 21:45:24 -0500

    Master
    Master

---
+++ Assets/Mainm.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Mainm : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void Escena_uno()
+    {
+        SceneManager.LoadScene("Punto 1");
+
+    }
+
+}
---
+++ Assets/Mastermain.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Mastermain : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void Escena_uno ()
+    {
+        SceneManager.LoadScene("Punto 1");
+    }
+    public void Escena_dos()
+    {
+        SceneManager.LoadScene("Punto 2");
+    }
+    public void Escena_tres()
+    {
+        SceneManager.LoadScene("Punto 3");
+    }
+    public void Escena_cuatro()
+    {
+        SceneManager.LoadScene("Punto 4");
+    }
+    public void Salir()
+    {
+        Application.Quit();
+    }
+
+}
